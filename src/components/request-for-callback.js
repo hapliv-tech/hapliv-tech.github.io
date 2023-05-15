e9aaa9cc0a45b2df5fd903181586aed108ff116f
Hapliv Tech <haplivdentalclinic@gmail.com>
2023-05-16 01:23:10 +0530

    form changes on invisalign page

--- src/components/request-for-callback.js
+++ src/components/request-for-callback.js
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import { useState } from 'react';
 async function saveFormData(data, src, cta, url) {
     data["email"] = data["email"] ? data["email"] : '';
     data["appointment_for"] = "Clicked " + cta + " from " + src;
@@ -12,8 +13,16 @@ async function saveFormData(data, src, cta, url) {
 }
 
 export default function RequestForCallback({ src, cta, url, callback }) {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => saveFormData(data, src, cta, 'https://hapliv-api.cyclic.app/appointments', callback);
+    const { register, handleSubmit, watch, formState: { errors }, reset } = useForm();
+    const [loading, setLoading] = useState(false);
+    const onSubmit = data => {
+        setLoading(true);
+        const response = saveFormData(data, src, cta, 'https://hapliv-api.cyclic.app/appointments', callback);
+        reset();
+        alert('Callback request submitted successfully');
+        setLoading(false);
+        return response;
+    }
     const fields = [
         {
             type: "text", name: "patient_name", required: true, label: "Name", validation: {
@@ -29,14 +38,14 @@ export default function RequestForCallback({ src, cta, url, callback }) {
     ];
     return <> <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col p-4 md:flex-row">
         {fields.map((field, index) => {
-            return <>
-                <div className="w-full px-3 mb-6 md:w md:mb-0">
+            return (
+                <div className="w-full px-3 mb-6 md:w md:mb-0" key={field.name + 'div' + index}>
                     <label htmlFor={field.name} key={field.name + 'label' + index} className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase">{field.label}</label>
                     <input className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border rounded appearance-none focus:outline-none focus:bg-white invalid:border-red-500" type={field.type} autoComplete={field.autoComplete} key={field.name + index}
                         {...register(field.name, field.validation)} />
                     {errors[field.name] && <Error message={errors[field.name].message} />}
                 </div>
-            </>
+            )
 
         })}
         <div className="w-full px-3 mb-6 md:w md:mb-0">
