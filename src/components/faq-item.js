db67381baddd8ddd04a839ae61b3c1dd3686d1fe
Hapliv Tech <haplivdentalclinic@gmail.com>
2024-04-18 00:03:51 +0530

    fix for error finding for attr

--- src/components/faq-item.js
+++ src/components/faq-item.js
@@ -12,8 +12,8 @@ export default function FaqItem({ faq }) {
          <div className="relative p-2 m-2 leading-8 text-left border card">
          <input type="checkbox" className="absolute opacity-0 peer" id={faq.question}></input>
          {/* Questions */}
-         <label for={faq.question} className="flex items-center text-xl font-bold text-left text-orange-600 basis-3/4 md:text-2xl question">Q. {faq.question}</label>
-         <label for={faq.question} className="ml-2 absolute top-[30px] right-[10px] rotate-0 peer-checked:rotate-180 duration-200"><FaAngleDown></FaAngleDown></label>
+         <label htmlFor={faq.question} className="flex items-center text-xl font-bold text-left text-orange-600 basis-3/4 md:text-2xl question">Q. {faq.question}</label>
+         <label htmlFor={faq.question} className="ml-2 absolute top-[30px] right-[10px] rotate-0 peer-checked:rotate-180 duration-200"><FaAngleDown></FaAngleDown></label>
          <p className="overflow-hidden text-base md:text-xl answer max-h-0 peer-checked:max-h-full">A. {faq.answers[0]}</p>
      </div>
     );
@@ -24,8 +24,8 @@ export function InvisalignFaqItem({ faq }) {
         <div className="relative p-2 m-2 leading-8 text-left border card">
             <input type="checkbox" className="absolute opacity-0 peer" id={faq.question}></input>
             {/* Questions */}
-            <label for={faq.question} className="flex items-center text-xl font-bold text-left text-orange-600 basis-3/4 md:text-2xl question">Q. {faq.question}</label>
-            <label for={faq.question} className="ml-2 absolute top-[30px] right-[10px] rotate-0 peer-checked:rotate-180 duration-200"><FaAngleDown></FaAngleDown></label>
+            <label htmlFor={faq.question} className="flex items-center text-xl font-bold text-left text-orange-600 basis-3/4 md:text-2xl question">Q. {faq.question}</label>
+            <label htmlFor={faq.question} className="ml-2 absolute top-[30px] right-[10px] rotate-0 peer-checked:rotate-180 duration-200"><FaAngleDown></FaAngleDown></label>
             <p className="overflow-hidden text-base md:text-xl answer max-h-0 peer-checked:max-h-full">A. {faq.answers[0]}</p>
         </div>
     );
@@ -37,8 +37,8 @@ export function AppointmentPageFAQItem({ faq }) {
         <div className="relative p-2 m-2 leading-8 text-left border card">
             <input type="checkbox" className="absolute opacity-0 peer" id={faq.question}></input>
             {/* Questions */}
-            <label for={faq.question} className="flex items-center text-lg font-bold text-left text-[#5A09A4] basis-3/4 md:text-lg question">Q. {faq.question}</label>
-            <label for={faq.question} className="ml-2 absolute top-[30px] right-[10px] rotate-0 peer-checked:rotate-180 duration-200"><FaAngleDown></FaAngleDown></label>
+            <label htmlFor={faq.question} className="flex items-center text-lg font-bold text-left text-[#5A09A4] basis-3/4 md:text-lg question">Q. {faq.question}</label>
+            <label htmlFor={faq.question} className="ml-2 absolute top-[30px] right-[10px] rotate-0 peer-checked:rotate-180 duration-200"><FaAngleDown></FaAngleDown></label>
             <p className="overflow-hidden text-base md:text-lg answer max-h-0 peer-checked:max-h-full">A. {faq.answers[0]}</p>
         </div>
     );
